5b336a608bf6345c48ae396405339321852b7da6
Matt Seaton <mattseaton22@gmail.com>
2024-03-25 19:26:00 -0500

    Moved dictionaries inside function bodies

--- addons/humanizer/scripts/assets/physical_skeleton.gd
+++ addons/humanizer/scripts/assets/physical_skeleton.gd
@@ -5,38 +5,6 @@ var helper_vertex: Array
 var layers
 var mask
 
-var next_limb_bone = {
-	&'Hips': &'UpperChest',
-	&'UpperChest': &'Neck',
-	&'Shoulder': &'UpperArm',
-	&'UpperArm': &'LowerArm',
-	&'LowerArm': &'Hand',
-	&'UpperLeg': &'LowerLeg',
-	&'LowerLeg': &'Foot',
-	&'Neck': &'Head',
-	&'Hand': &'MiddleProximal',
-	&'Foot': &'Toes',
-}
-
-#refer to mpfb2_plugin.data/mesh_metadata/hm08.mirror for opposites
-var vertex_names = {
-	&"LeftUpperArmFront" = 8114,
-	&"LeftUpperArmBack" = 8330,
-	&"RightUpperArmFront" = 1426,
-	&"RightUpperArmBack" = 1658,
-	&"LeftLowerArmFront" = 10541,
-	&"LeftLowerArmBack" = 10110,
-	&"RightLowerArmFront" = 3876,
-	&"RightLowerArmBack" = 3442,
-	&"LeftLowerLegFront" = 11325,
-	&"LeftLowerLegBack" = 11339,
-	&"RightLowerLegFront" = 4707,
-	&"RightLowerLegBack" = 4721,
-	&"LeftUpperLegFront" = 11116,
-	&"LeftUpperLegBack" = 13340,
-	&"RightUpperLegFront" = 4498,
-	&"RightUpperLegBack" = 6744,
-}
 
 func _init(_skeleton: Skeleton3D, _helper_vertex, _layers, _mask):
 	skeleton = _skeleton
@@ -51,6 +19,19 @@ func run() -> void:
 	
 	_add_collider(&'Head', null, 'sphere')
 	
+	var next_limb_bone = {
+		&'Hips': &'UpperChest',
+		&'UpperChest': &'Neck',
+		&'Shoulder': &'UpperArm',
+		&'UpperArm': &'LowerArm',
+		&'LowerArm': &'Hand',
+		&'UpperLeg': &'LowerLeg',
+		&'LowerLeg': &'Foot',
+		&'Neck': &'Head',
+		&'Hand': &'MiddleProximal',
+		&'Foot': &'Toes',
+	}
+
 	for bone in next_limb_bone:
 		var shape := 'capsule'
 		if 'Hand' in bone or 'Foot' in bone or 'Hips' in bone or 'Chest' in bone:
@@ -111,6 +92,25 @@ func _add_collider(bone, next=null, shape='capsule') -> void:
 			collider.shape.size.z
 
 func distance_between_vertices(bone: String):
+	#refer to mpfb2_plugin.data/mesh_metadata/hm08.mirror for opposites
+	var vertex_names = {
+		"LeftUpperArmFront" = 8114,
+		"LeftUpperArmBack" = 8330,
+		"RightUpperArmFront" = 1426,
+		"RightUpperArmBack" = 1658,
+		"LeftLowerArmFront" = 10541,
+		"LeftLowerArmBack" = 10110,
+		"RightLowerArmFront" = 3876,
+		"RightLowerArmBack" = 3442,
+		"LeftLowerLegFront" = 11325,
+		"LeftLowerLegBack" = 11339,
+		"RightLowerLegFront" = 4707,
+		"RightLowerLegBack" = 4721,
+		"LeftUpperLegFront" = 11116,
+		"LeftUpperLegBack" = 13340,
+		"RightUpperLegFront" = 4498,
+		"RightUpperLegBack" = 6744,
+	}
 	var vertex_1 := bone + &'Front'
 	var vertex_2 := bone + &'Back'
 	return helper_vertex[vertex_names[vertex_1]].distance_to(helper_vertex[vertex_names[vertex_2]])
