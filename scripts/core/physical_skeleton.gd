a9373a5e7dd4430b8151d32fa44cb1420e85c589
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2024-04-24 13:02:57 -0500

    adjust contraints, add neck collider, remove shoulders

--- scripts/core/physical_skeleton.gd
+++ scripts/core/physical_skeleton.gd
@@ -27,12 +27,12 @@ func run() -> void:
 	var next_limb_bone = {
 		&'Hips': &'UpperChest',
 		&'UpperChest': &'Neck',
-		&'Shoulder': &'UpperArm',
+		#&'Shoulder': &'UpperArm', ## unity default ragdoll does not include
 		&'UpperArm': &'LowerArm',
 		&'LowerArm': &'Hand',
 		&'UpperLeg': &'LowerLeg',
 		&'LowerLeg': &'Foot',
-		#&'Neck': &'Head',  ## Don't think we need this collider
+		&'Neck': &'Head',  ## Don't think we need this collider
 		&'Hand': &'MiddleDistal',
 		&'Foot': &'Toes',
 	}
@@ -73,6 +73,7 @@ func _add_collider(bone, next=null, shape:=ColliderShape.CAPSULE) -> void:
 			var bounds = get_sphere_vertex_bounds(bone)
 			collider.shape.radius = bounds.radius
 			collider.global_position = bounds.center
+			collider.global_transform = skeleton.global_transform * collider.global_transform
 	else:
 		var next_id: int = skeleton.find_bone(next)
 		var next_position: Vector3 = skeleton.get_bone_global_pose(next_id).origin 
@@ -124,13 +125,15 @@ func add_joint(phys_bone:PhysicalBone3D,bone_name:String):
 		"LeftUpperArm" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=60,"joint_constraints/twist_span"=30}],
 		"RightUpperArm" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=60,"joint_constraints/twist_span"=30}],
 		#"Hips" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
-		"UpperChest" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=100,"joint_constraints/twist_span"=50}],
+		"UpperChest" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=80,"joint_constraints/twist_span"=30}],
 		"LeftUpperLeg" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
 		"RightUpperLeg" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
 		"LeftFoot" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
 		"RightFoot" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
 		"LeftHand" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
 		"RightHand" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=30,"joint_constraints/twist_span"=30}],
+		"Neck" = [PhysicalBone3D.JOINT_TYPE_CONE,{"joint_constraints/swing_span"=20,"joint_constraints/twist_span"=20}],
+		
 	}
 	
 	if bone_name in joints:
@@ -156,63 +159,51 @@ func get_sphere_vertex_bounds(bone: String) -> Dictionary:
 func get_box_vertex_bounds(bone: String) -> Dictionary:
 	var vertex_names = {
 		"Hips" = {upper=4154, lower=4370, front=4110, back=11006, left=10899, right=4269},
-		"UpperChest" = {upper=1401, lower=4154, front=1890, back=1598, left=10602, right=3938},
+		"UpperChest" = {upper=1526, lower=4154, front=1890, back=1598, left=10602, right=3938},
 		"LeftFoot" = {upper=12818, lower=12877, front=13146, back=12442, left=12808, right=13300},
 		"RightFoot" = {upper=6221, lower=6280, front=6550, back=5845, left=6704, right=6211},
 		"LeftHand" = {upper=10489, lower=8929, front=9456,back=10535,left=9833,right=10306},
 		"RightHand" = {upper=3823 ,lower=2261 ,front=2788 ,back=3870 ,left=3638 ,right=3165 },
 		}
 		
-	var top : int = vertex_names[bone]['upper']
-	var bottom : int = vertex_names[bone]['lower']
-	var left : int = vertex_names[bone]['left']
-	var right : int = vertex_names[bone]['right']
-	var front : int = vertex_names[bone]['front']
-	var back : int = vertex_names[bone]['back']
+	var upper : Vector3 = helper_vertex[vertex_names[bone]['upper']]
+	var lower : Vector3 = helper_vertex[vertex_names[bone]['lower']]
+	var left : Vector3 = helper_vertex[vertex_names[bone]['left']]
+	var right : Vector3 = helper_vertex[vertex_names[bone]['right']]
+	var front : Vector3 = helper_vertex[vertex_names[bone]['front']]
+	var back : Vector3 = helper_vertex[vertex_names[bone]['back']]
 	
 		
 	var size := Vector3.ZERO
-	size.x = abs(helper_vertex[left].x - helper_vertex[right].x)
-	size.y = abs(helper_vertex[top].y - helper_vertex[bottom].y)
-	size.z = abs(helper_vertex[front].z - helper_vertex[back].z)
+	size.x = abs(left.x - right.x)
+	size.y = abs(upper.y - lower.y)
+	size.z = abs(front.z - back.z)
 	
 	var center := Vector3.ZERO
-	center.x = (helper_vertex[left].x + helper_vertex[right].x) * .5
-	center.y = (helper_vertex[top].y + helper_vertex[bottom].y) * .5
-	center.z = (helper_vertex[front].z + helper_vertex[back].z) * .5
+	center.x = (left.x + right.x) * .5
+	center.y = (upper.y + lower.y) * .5
+	center.z = (front.z + back.z) * .5
 	
 	return {size=size, center=center}
 
 func get_capsule_vertex_bounds(bone: String) -> Dictionary:
 	#refer to mpfb2_plugin.data/mesh_metadata/hm08.mirror for opposites
 	var vertex_names = {
-		"LeftUpperArmFront" = 8114,
-		"LeftUpperArmBack" = 8330,
-		"RightUpperArmFront" = 1426,
-		"RightUpperArmBack" = 1658,
-		"LeftLowerArmFront" = 10541,
-		"LeftLowerArmBack" = 10110,
-		"RightLowerArmFront" = 3876,
-		"RightLowerArmBack" = 3442,
-		"LeftLowerLegFront" = 11325,
-		"LeftLowerLegBack" = 11339,
-		"RightLowerLegFront" = 4707,
-		"RightLowerLegBack" = 4721,
-		"LeftUpperLegFront" = 11116,
-		"LeftUpperLegBack" = 13340,
-		"RightUpperLegFront" = 4498,
-		"RightUpperLegBack" = 6744,
-		"NeckFront" = 791,
-		"NeckBack" = 856,
-		"LeftShoulderFront" = 8057,
-		"LeftShoulderBack" = 8281,
-		"RightShoulderFront" = 1365,
-		"RightShoulderBack" = 1609,
-		
+		"LeftUpperArm" = {front=8114, back=8330},
+		"RightUpperArm" = {front=1426, back=1658},
+		"LeftLowerArm" = {front=10541, back=10110}, 
+		"RightLowerArm" = {front=3876, back=3442}, 
+		"LeftLowerLeg" = {front=11325, back=11339},
+		"RightLowerLeg" = {front=4707, back=4721},
+		"LeftUpperLeg" = {front=11116, back=13340},
+		"RightUpperLeg" = {front=4498, back=6744},
+		"Neck" = {front=791, back=856},
+		"LeftShoulder" = {front=8057, back=8281},
+		"RightShoulder" = {front=1365, back=1609},
 	}
-	var vertex_1 : int = vertex_names[bone + &'Front']
-	var vertex_2 : int = vertex_names[bone + &'Back']
+	var front : Vector3 = helper_vertex[vertex_names[bone]['front']]
+	var back : Vector3 = helper_vertex[vertex_names[bone]['back']]
 	return {
-		'distance': helper_vertex[vertex_1].distance_to(helper_vertex[vertex_2]),
-		'center': 0.5 * (helper_vertex[vertex_1] + helper_vertex[vertex_2])
+		'distance': front.distance_to(back),
+		'center': 0.5 * (front + back)
 	}
