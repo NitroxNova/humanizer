df84c72d05ea387a42c5c0e2dcf9c467efa832d7
Matt Seaton <mattseaton22@gmail.com>
2024-05-02 00:00:42 -0500

    tpose conversion almost working again

--- scripts/core/animation_retargeter.gd
+++ scripts/core/animation_retargeter.gd
@@ -28,12 +28,12 @@ func convert_library() -> void:
 	skeleton.reset_bone_poses()
 	var anim : AnimationPlayer = human.animator.get_child(0)
 
+	anim.add_animation_library('tpose', load("res://addons/humanizer/data/animations/tpose.glb"))
+	anim.play('tpose/tpose')
 	var apose_rest = []
 	for bone in skeleton.get_bone_count():
 		#apose_rest.append(skeleton.get_bone_global_rest(bone).basis.get_rotation_quaternion())
 		apose_rest.append(skeleton.get_bone_rest(bone).basis.get_rotation_quaternion())
-	anim.add_animation_library('tpose', load("res:/	/addons/humanizer/data/animations/tpose.glb"))
-	anim.play('tpose/tpose')
 
 	# Wait for pose to take.  It could give bad results if skinning shader gets delayed
 	await get_tree().create_timer(1.).timeout
@@ -61,7 +61,7 @@ func convert_library() -> void:
 						continue
 					var rotation : Quaternion = clip.track_get_key_value(track, key)
 					var tpose_to_apose : Quaternion = apose_rest[bone].inverse() * tpose_rest[bone]
-					new_clip.track_insert_key(track, t, rotation * tpose_to_apose)
+					new_clip.track_insert_key(track, t, (rotation * tpose_to_apose).normalized())
 				else:  # Just copy other track types
 					new_clip.track_insert_key(track, t, clip.track_get_key_value(track, key))
 	var libname = library.resource_path.get_file().get_basename()
