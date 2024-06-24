d95f3eb1adf378ad0f667bc1a8955c62694e8f17
Nitrox Nova <nitroxnova@NitroxNova.bigchungus>
2024-06-24 04:38:56 -0500

    shapekey data in smaller files

--- scripts/preprocessing/shapekey_reader.gd
+++ scripts/preprocessing/shapekey_reader.gd
@@ -7,6 +7,11 @@ var shapekey_data := {}
 # Process the shape key data.
 func run():
 	print('Collecting shape key data from target files')
+	
+	#clean shapekey folder
+	for path in OSPath.get_files("res://addons/humanizer/data/resources/shapekey/"):
+		DirAccess.remove_absolute(path)
+	
 	shapekey_data = {}
 	
 	# Load helper mesh and index vertices
@@ -27,20 +32,28 @@ func run():
 		_get_shape_keys(path + 'targets/')
 	
 	shapekey_data.macro_shapekeys = []
-	for sk in shapekey_data.shapekeys:
-		if sk.begins_with('african') or sk.begins_with('asian') or sk.begins_with('caucasian')\
-				or sk.begins_with('female') or sk.begins_with('male') or sk.begins_with('universal'):
-			shapekey_data.macro_shapekeys.append(sk)
+	for category in shapekey_data.shapekeys:
+		for sk in shapekey_data.shapekeys[category]:
+			if sk.begins_with('african') or sk.begins_with('asian') or sk.begins_with('caucasian')\
+					or sk.begins_with('female') or sk.begins_with('male') or sk.begins_with('universal'):
+				shapekey_data.macro_shapekeys.append(sk)
+			
+	var base_data_file := FileAccess.open("res://addons/humanizer/data/resources/shapekey/_base_data.dat", FileAccess.WRITE)
+	base_data_file.store_var(shapekey_data.basis)
+	base_data_file.store_var(shapekey_data.macro_shapekeys)
+	base_data_file.close()
 	
-	var file := FileAccess.open("res://addons/humanizer/data/resources/shapekeys.dat", FileAccess.WRITE)
-	file.store_var(shapekey_data)
-	file.close()
+	for category in shapekey_data.shapekeys:
+		var file := FileAccess.open("res://addons/humanizer/data/resources/shapekey/" + category + ".dat", FileAccess.WRITE)
+		file.store_var(shapekey_data.shapekeys[category])
+		file.close()
+		
 	HumanizerUtils._shapekey_data = HumanizerUtils.get_shapekey_data()
 	print('Finished collecting shapekey data')
 
-func _process_shapekey(path: String):
+func _process_shapekey(path: String, category:String):
 	var shape_name = path.get_file().get_basename()
-	shapekey_data.shapekeys[shape_name] = {}
+	shapekey_data.shapekeys[category][shape_name] = {}
 	var target_file = FileAccess.open(path, FileAccess.READ)
 
 	while target_file.get_position() < target_file.get_length():
@@ -51,12 +64,17 @@ func _process_shapekey(path: String):
 		var sk_index = int(floats[0])
 		var sk_coords = Vector3(floats[1],floats[2],floats[3])
 		sk_coords *= 0.1 # Original scale
-		shapekey_data.shapekeys[shape_name][sk_index] = sk_coords
+		shapekey_data.shapekeys[category][shape_name][sk_index] = sk_coords
 		
-func _get_shape_keys(path):
+func _get_shape_keys(path, category=""):
 	var contents = OSPath.get_contents(path)
 	for dir in contents.dirs:
-		_get_shape_keys(dir)
+		if category == "":
+			_get_shape_keys(dir,dir.get_file())
+		else:
+			_get_shape_keys(dir,category + "_" + dir.get_file())
 	for file in contents.files:
 		if file.get_extension() == 'target':
-			_process_shapekey(file)
+			if category not in shapekey_data.shapekeys:
+				shapekey_data.shapekeys[category] = {}
+			_process_shapekey(file,category)
