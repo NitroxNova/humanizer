1d82dbc7b2b46942fc45c9ee91cb922f332d44ad
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2024-11-12 23:14:49 -0600

    dont generate config resource, let user create if wanted

--- scripts/core/humanizer_global_config.gd
+++ scripts/core/humanizer_global_config.gd
@@ -18,3 +18,10 @@ func _init() -> void:
 	## Just using this to have a static variable which is also exported.
 	if Instance == null:
 		Instance = self
+
+func _ready():
+	var config_path = "res://humanizer_global_config.res"
+	if FileAccess.file_exists(config_path):
+		_config = load(config_path)
+	if _config == null:
+		_config = HumanizerConfig.new()
