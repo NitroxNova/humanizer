7b7744b011b85698d3dbd9dd42eb97504d1d13cd
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2024-11-16 21:34:06 -0600

    fill check boxes from settings or existing resource

--- scripts/core/asset_importer.gd
+++ scripts/core/asset_importer.gd
@@ -2,12 +2,12 @@
 class_name HumanizerAssetImporter 
 extends Node
 
-signal file_changed(path:String)
+signal file_changed()
 
 @export_file("*.mhclo") var asset_path = '':
 	set(value):
 		asset_path = value
-		file_changed.emit(value)
+		file_changed.emit()
 		#print("file changed")
 
 
--- scripts/inspector/asset_importer_inspector.gd
+++ scripts/inspector/asset_importer_inspector.gd
@@ -28,15 +28,41 @@ func _parse_category(_importer, category):
 			checkbox.text = slot
 			container.add_child(checkbox)
 			slot_boxes[slot+slots_cat.suffix] = checkbox
+		if not importer.file_changed.is_connected(fill_options):
+			importer.file_changed.connect(fill_options)
 		inspector.get_node('%SlotsContainer').add_child(container)
 		#print(slots_cat.category)
-
+		
+	fill_options()
 	inspector.get_node('%ImportButton').pressed.connect(import_asset)
 
-func fill_options_from_file():
-	pass
-	#for slot in slot_boxes:
-		
+	
+func fill_options():
+	reset_checkboxes()
+	var json_path = get_import_settings_path()
+	if FileAccess.file_exists(json_path):
+		#print("loading json")
+		var settings = HumanizerUtils.read_json(json_path)
+		for slot in settings.slots:
+			slot_boxes[slot].button_pressed = true
+	else:
+		#print("loading resource")
+		#try new resource naming convention first
+		var res_path = get_equipment_resource_path()
+		if not FileAccess.file_exists(res_path):
+			#old naming convention has to be loaded from mhclo
+			var mhclo := MHCLO.new()
+			mhclo.parse_file(importer.asset_path)
+			res_path = importer.asset_path.get_base_dir()
+			res_path = res_path.path_join(mhclo.resource_name + ".res")
+			print(res_path)
+		var equip_res : HumanizerEquipmentType = load(res_path)
+		for slot in equip_res.slots:
+			slot_boxes[slot].button_pressed = true
+
+func reset_checkboxes():
+	for box in slot_boxes.values():
+		box.button_pressed = false		
 
 func import_asset():
 	#print("importing asset")
@@ -48,9 +74,18 @@ func import_asset():
 			slot_list.append(slot_name)
 	import_settings.slots = slot_list
 	import_settings.mhclo = importer.asset_path
-	var save_file = importer.asset_path.get_basename()
-	save_file += ".import_settings.json"
-	print(save_file)
+	var save_file = get_import_settings_path()
 	HumanizerUtils.save_json(save_file,import_settings)
 	HumanizerEquipmentImportService.import(save_file)
+	
+func get_import_settings_path()->String:
+	var save_file = importer.asset_path.get_basename()
+	save_file += ".import_settings.json"
+	#print(save_file)
+	return save_file
+
+func get_equipment_resource_path()->String:
+	var res_path = importer.asset_path.get_basename()
+	res_path += ".res"
+	return res_path
 	
