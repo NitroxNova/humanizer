584d102e3583ea207c50551940b2f830a27ac894
mikeysasse <michaellavonsasse@gmail.com>
2024-11-19 22:54:46 -0600

    add basic profiler output to logger, can be used for on screen text with print_profile()

--- scripts/utils/HumanizerLogger.gd
+++ scripts/utils/HumanizerLogger.gd
@@ -4,6 +4,9 @@ enum LogLevel { INFO, DEBUG }
 static var log_level = LogLevel.INFO
 static var log_file: FileAccess = FileAccess.open("user://humanizer.log", FileAccess.READ_WRITE)
 
+static var profile_db_mutex := Mutex.new()
+static var profile_db = {}
+
 static func _log(category: String, string: String):
 	var message = "[humanizer " + category + "] " + str(string)
 	print(message)
@@ -25,8 +28,32 @@ static func profile(name: String, callable: Callable):
 		return callable.call()
 	var timer = Time.get_ticks_msec()
 	var obj = callable.call()
-	_log("profile", name + ": " + str(Time.get_ticks_msec() - timer) + "ms ")
+	var elapsed = Time.get_ticks_msec() - timer
+	# _log("profile", name + ": " + str(elapsed) + "ms ")
+
+	profile_db_mutex.lock()
+	if not profile_db.has(name):
+		profile_db[name] = []
+	profile_db[name].append(elapsed)
+	profile_db_mutex.unlock()
+
 	return obj
+
+static func print_profile() -> String:
+	profile_db_mutex.lock()
+	var s := ""
+
+	s += "### PROFILE ###\n"
+	for key in profile_db.keys():
+		var profile_times = profile_db[key]
+		var count = len(profile_times)
+		var time = 0
+		for i in profile_times:
+			time += i
+		s += key + " " + str(time / count) + "ms\n"
+
+	profile_db_mutex.unlock()
+	return s
 
 static func log_stack_trace(message):
 	HumanizerLogger.info(message + "\n" + str(get_stack()) + "\n") # todo format it better
