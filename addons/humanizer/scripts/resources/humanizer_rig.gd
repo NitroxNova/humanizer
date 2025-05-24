4ad1f71846f1adbe3cd43f2195938dea011d2d77
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2025-05-24 01:38:41 -0500

    combine rig data into resource

--- addons/humanizer/scripts/resources/humanizer_rig.gd
+++ addons/humanizer/scripts/resources/humanizer_rig.gd
@@ -1,26 +1,19 @@
 extends Resource
 class_name HumanizerRig
 
-@export var mh_json_path: String
-@export var mh_weights_path: String
-@export var bone_weights_json_path: String
-@export var config_json_path: String
-@export var skeleton_path: String
-@export var skeleton_retargeted_path: String
-@export var rigged_mesh_path: String
+@export var skeleton : PackedScene #saving the skeleton3d node doesnt work, it gets reset when you open it
+@export var config : Array #bone position info, from json
+@export var weights : Array #weights and bones arrays, indexed to helper vertex
 
 func load_skeleton() -> Skeleton3D:
-	return HumanizerResourceService.load_resource(skeleton_path).instantiate()
+	return skeleton.instantiate()
 
-func load_retargeted_skeleton() -> Skeleton3D:
-	return HumanizerResourceService.load_resource(skeleton_retargeted_path).instantiate()
-
-func load_bone_weights() -> Dictionary:
-	var weights: Dictionary = HumanizerResourceService.load_resource(mh_weights_path).weights
-	for in_name:String in weights.keys():
-		if ':' not in in_name:
-			continue
-		var out_name = in_name.replace(":", "_")
-		weights[out_name] = weights[in_name]
-		weights.erase(in_name)
-	return weights
+#func load_bone_weights() -> Dictionary:
+	#var weights: Dictionary = HumanizerResourceService.load_resource(mh_weights_path).weights
+	#for in_name:String in weights.keys():
+		#if ':' not in in_name:
+			#continue
+		#var out_name = in_name.replace(":", "_")
+		#weights[out_name] = weights[in_name]
+		#weights.erase(in_name)
+	#return weights
