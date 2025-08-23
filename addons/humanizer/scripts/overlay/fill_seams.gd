c26113505b80e5507f9fc992c3aaadc76d27ca95
NitroxNova <nitroxnova.games@gmail.com>
2025-08-23 03:59:37 -0500

    fill seems shader

---
+++ addons/humanizer/scripts/overlay/fill_seams.gd
@@ -0,0 +1,24 @@
+@tool
+extends HumanizerOverlay
+class_name HumanizerOverlayFillSeams
+
+@export var base_image : HumanizerOverlay
+@export var margin = 1
+
+func get_texture_node(target_size:Vector2,mesh_arrays:Array): 
+	
+	var base_image_node = base_image.get_texture_node(target_size,mesh_arrays)
+	var sub_viewport = SubViewport.new()
+	sub_viewport.transparent_bg = true
+	sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+	sub_viewport.size = target_size
+	sub_viewport.add_child(base_image_node)
+	var node = SubViewportContainer.new()
+	node.add_child(sub_viewport)
+	var shader_material = ShaderMaterial.new()
+	shader_material.shader = HumanizerResourceService.load_resource("res://addons/humanizer/scripts/overlay/fill_seams.gdshader") 
+	shader_material.set_shader_parameter("margin",margin)
+	#shader_material.set_shader_parameter("blend_amount",blend_amount)
+	
+	node.material = shader_material
+	return node
